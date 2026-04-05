24e40de342909761edf9b45ff6d02e3d7a8ff8c4
kanykeizulumova <zulumovakanykei54@gmail.com>
2026-04-05 23:21:36 +0600

    remove dist from tracking

--- christmas-shop/dist/gifts.821f41957b23e6ee98ce.js
+++
@@ -1,2 +0,0 @@
-(()=>{"use strict";console.log("Christmas Shop - Gifts page loaded")})();
-//# sourceMappingURL=gifts.821f41957b23e6ee98ce.js.map
--- christmas-shop/dist/index.900eb10f527327f25ed4.js
+++
@@ -1,2 +0,0 @@
-(()=>{"use strict";console.log("Christmas Shop - Home page loaded")})();
-//# sourceMappingURL=index.900eb10f527327f25ed4.js.map
