364e79a1d15ee502769b803d2ba3d481403ff17a
kanykeizulumova <zulumovakanykei54@gmail.com>
2026-04-05 23:19:09 +0600

    deploy dist to gh-pages

---
+++ christmas-shop/dist/gifts.821f41957b23e6ee98ce.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";console.log("Christmas Shop - Gifts page loaded")})();
+//# sourceMappingURL=gifts.821f41957b23e6ee98ce.js.map
---
+++ christmas-shop/dist/index.900eb10f527327f25ed4.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";console.log("Christmas Shop - Home page loaded")})();
+//# sourceMappingURL=index.900eb10f527327f25ed4.js.map
