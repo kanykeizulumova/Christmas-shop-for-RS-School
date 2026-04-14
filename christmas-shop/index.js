0a864429fcd7a024461d7f1e7d646514194b5fa0
kanykeizulumova <zulumovakanykei54@gmail.com>
2026-04-14 16:19:34 +0600

    feat: Implementation of the Timer on the home page

--- christmas-shop/index.js
+++ christmas-shop/index.js
@@ -28,3 +28,37 @@ window.addEventListener('resize', () => {
         body.classList.remove('lock');
     }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const deadline = new Date('2026-12-31T23:59:59Z');
+
+    const elDays = document.querySelector('.timer__days');
+    const elHours = document.querySelector('.timer__hours');
+    const elMinutes = document.querySelector('.timer__minutes');
+    const elSeconds = document.querySelector('.timer__seconds');
+
+    const declensionNum = (num, words) => {
+        return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][num % 10 < 5 ? num % 10 : 5]];
+    };
+
+    const updateTimer = () => {
+        const now = new Date().getTime();
+        const diff = Math.max(0, deadline.getTime() - now);
+
+        const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+        const minutes = Math.floor((diff / (1000 * 60)) % 60);
+        const seconds = Math.floor((diff / 1000) % 60);
+
+        elDays.textContent = days;
+        elHours.textContent = hours;
+        elMinutes.textContent = minutes;
+        elSeconds.textContent = seconds;
+
+        if (diff === 0) {
+            clearInterval(timerId);
+        }
+    };
+    updateTimer();
+    const timerId = setInterval(updateTimer, 1000);
+});
