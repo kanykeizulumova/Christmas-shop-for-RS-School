c2605c67def2cad418002bbf64c17a25a723fdc8
kanykeizulumova <zulumovakanykei54@gmail.com>
2026-04-14 18:58:00 +0600

    refactor: add js to gifts.js, so other functions do not interrupt work on gifts page

---
+++ christmas-shop/gifts.js
@@ -0,0 +1,19 @@
+const burger = document.getElementById('burger');
+const menu = document.getElementById('nav-menu');
+const body = document.body;
+const navLinks = document.querySelectorAll('.nav-link');
+
+function toggleMenu() {
+    burger.classList.toggle('active');
+    menu.classList.toggle('open');
+    body.classList.toggle('lock');
+}
+burger.addEventListener('click', toggleMenu);
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        if (menu.classList.contains('open')) {
+            toggleMenu();
+        }
+    });
+});
